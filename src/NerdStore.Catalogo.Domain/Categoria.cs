f0686144f5b6a21749e67be8b9087ec554ad051f
Karen Melo <karen_melo@live.com>
2021-11-10 01:14:21 -0300

    Inclusões de mapeamentos, classe concreta e interface de ProdutoRespoitory, Context

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -12,6 +12,11 @@ namespace NerdStore.Catalogo.Domain
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
 
+        //EF Relation
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
+
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
